c89d7cb5539e1cf01afc64a6d348fabe93398a6f
Gabriel Gonzalez <Gabriel439@gmail.com>
2019-05-11 10:10:05 -0700

    Build against `semigroups-0.19` (#54)
    Also drop support for GHC 7.6 since our dependencies no
    longer support it

--- release.nix
+++ release.nix
@@ -13,16 +13,19 @@ let
     packageOverrides = pkgs: {
       haskellPackages = pkgs.haskellPackages.override {
         overrides = haskellPackagesNew: haskellPackagesOld: {
-          optparse-applicative = haskellPackagesNew.callPackage ./optparse-applicative.nix { };
-
           optparse-generic = haskellPackagesNew.callPackage ./default.nix { };
         };
       };
     };
   };
 
-  pkgs =
-    import <nixpkgs> { inherit config; };
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOs/nixpkgs/archive/312a059bef8b29b4db4e73dc02ff441cab7bb26d.tar.gz";
+
+    sha256 = "1j52yvkhw1inp6ilpqy81xv1bbwgwqjn0v9647whampkqgn6dxhk";
+  };
+
+  pkgs = import nixpkgs { inherit config; };
 
 in
   { optparse-generic = pkgs.haskellPackages.optparse-generic;
