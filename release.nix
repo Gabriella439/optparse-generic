2389fe65bf080165607e87616f5d1be0ab291f33
Gabriel Gonzalez <Gabriel439@gmail.com>
2021-09-01 22:13:30 -0700

    Version 1.4.4 → 1.4.5

--- release.nix
+++ release.nix
@@ -1,31 +1,18 @@
-# You can build this repository using Nix by running:
-#
-#     $ nix-build -A optparse-generic release.nix
-#
-# You can also open up this repository inside of a Nix shell by running:
-#
-#     $ nix-shell -A optparse-generic.env release.nix
-#
-# ... and then Nix will supply the correct Haskell development environment for
-# you
 let
   config = {
     packageOverrides = pkgs: {
       haskellPackages = pkgs.haskellPackages.override {
-        overrides = haskellPackagesNew: haskellPackagesOld: {
-          optparse-generic = haskellPackagesNew.callPackage ./default.nix { };
-
-          optparse-applicative =
-            haskellPackagesNew.optparse-applicative_0_16_0_0;
+        overrides = pkgs.haskell.lib.packageSourceOverrides {
+          optparse-generic = ./.;
         };
       };
     };
   };
 
   nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/NixOs/nixpkgs/archive/d8e0ade97ad89cd7ea4452e41b4abcaf7e04a8b7.tar.gz";
+    url = "https://github.com/NixOs/nixpkgs/archive/3e09410da091230c725faa80af462557c94d11d9.tar.gz";
 
-    sha256 = "1rm6z9cch0kld1742inpsch06n97qik30a3njglvq52l8g9xw2jj";
+    sha256 = "089kf5sv6qqnry9cya9psjp0y65daiks2ypxmglm2wxs6c7zm4zr";
   };
 
   pkgs = import nixpkgs { inherit config; };
